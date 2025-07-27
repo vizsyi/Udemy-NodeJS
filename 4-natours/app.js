bd571a3f18f5f811e0a2c7bbc41fcf04a089ed07
Vizsy István <steve.visy@gmail.com>
2025-07-27 12:02:32 +0200

    #4: serving static files

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -11,6 +11,8 @@ app.use(morgan("dev"));
 
 app.use(express.json()); //to use to read the body of request
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
