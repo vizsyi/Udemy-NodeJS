588ddb0f8a443a587ff24e97234243f7138eff1c
Vizsy István <steve.visy@gmail.com>
2025-08-04 13:03:35 +0200

    #4: getAllTours method with pagination, filtering, sorting and selecting

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -4,14 +4,66 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+
+    /*
+    const quer = Tour.find()
+      .where("duration")
+      .gte(5)
+      .equals(5)
+      .where("difficulty")
+      .equals("easy");
+    */
+
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((exField) => delete queryObj[exField]);
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // 3) Field limiting (Selecting)
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (page > 1) {
+      //if (req.query.page)
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
+
+    const tours = await query;
+
     res.status(200).json({
       status: "success",
       results: tours.length,
       data: { tours },
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(404).json({
       status: "fail",
       message: err.message,
     });
